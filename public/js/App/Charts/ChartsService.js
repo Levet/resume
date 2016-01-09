128f132327c5765b09b5f76be4f72e37c48daf90
jordan <jordan@onlineimage.com>
2016-01-08 19:52:35 -0700

    added disclaimer to api call

--- public/js/App/Charts/ChartsService.js
+++ public/js/App/Charts/ChartsService.js
@@ -3,7 +3,7 @@
 		var ChartsService = {};
 
 		ChartsService.stocks = function(){
-			return $http
+			return $http  // Uses a free api key. No need to hide.
 			.get('https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json?start_date=2014-07-27&end_date=2015-07-27&column_index=4&api_key=bdLZ1oFvnTSoRyZAFd_K')
 			.then(function(response){
 				return response;
